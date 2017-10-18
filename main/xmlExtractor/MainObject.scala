e705573c0332e06053c0bb9ad73fcc26e87d08a3
zied sendi <zied.sendi.ensi@gmail.com>
2017-10-18 14:10:27 +0200

    Main Test

--- main/xmlExtractor/MainObject.scala
+++ main/xmlExtractor/MainObject.scala
@@ -5,14 +5,10 @@ object MainObject {
 
   /**
    * Test the Parser 
-   */
-  
+   */ 
   def main(args :Array[String]): Unit = {
     val context = new AttributeTraitImpl()
     val listArtist = context.getListArtist()   
-    listArtist.foreach(x => println("Artist :" + x +"\n" ))    
-   
-    
-  }
-  
+    listArtist.foreach(x => println("Artist :" + x +"\n" ))       
+  }  
 }
